2ee6a3985426d9a0031d9bdfeb4905374b372afe
Mikhail <miksmirnov01@gmail.com>
2023-06-01 16:40:13 +0700

    back 8000

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   devServer: {
-    port: 8001,
+    port: 8000,
   },
   transpileDependencies: true,
   css: {
