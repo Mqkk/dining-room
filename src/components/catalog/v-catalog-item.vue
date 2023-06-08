365a7a9f7a70be444f7d33df6bb0959532a2fc38
Mikhail <miksmirnov01@gmail.com>
2023-06-08 14:57:09 +0700

    ./././/.

--- src/components/catalog/v-catalog-item.vue
+++ src/components/catalog/v-catalog-item.vue
@@ -6,7 +6,7 @@
         <img
           class="img"
           :alt="product_data.good__name"
-          :src="product_data.good__image"
+          :src="`../../../../../..${product_data.good__image}`"
         />
       </div>
       <div class="catalog-item__name">{{ product_data.good__name }}</div>
